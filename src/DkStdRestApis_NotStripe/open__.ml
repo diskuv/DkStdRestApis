95d8c5a89acc52a2ff544435db25a03280a5b802
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-09-16 15:49:15 -0700

    Import Tr1Http_Std.Http

--- src/DkStdRestApis_NotStripe/open__.ml
+++ src/DkStdRestApis_NotStripe/open__.ml
@@ -1,9 +1,3 @@
-(* CHANGE NOTICE
-   1. Modify {!StandardExt.caseize} documentation and body to
-   translate any module name you see below when you add/change them. *)
-
-(* Used only by DkCoder since excluded in [./dune] *)
-
 module String = Tr1Stdlib_V414Base.String
 module Option = Tr1Stdlib_V414Base.Option
 module List = Tr1Stdlib_V414Base.List
@@ -15,8 +9,7 @@ module Format = Tr1Stdlib_V414CRuntime.Format
 module Stringext = Tr1String_Ext.Stringext
 module Map = Tr1Stdlib_V414Base.Map
 module Array = Tr1Stdlib_V414Base.Array
-
-(* Used by StripeExt *)
 module Printf = Tr1Stdlib_V414CRuntime.Printf
 module Float = Tr1Stdlib_V414Base.Float
 module Int64 = Tr1Stdlib_V414Base.Int64
+module Http = Tr1Http_Std.Http
